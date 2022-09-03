982b5b947aa503c727bcfa91d3b15a0d11cfd67e
ohshit <897927040@qq.com>
2022-09-03 23:05:30 +0800

    idea提交

---
+++ src/main/java/com/tydic/boot/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.tydic.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello1")
+    public String handle01(){
+        return "Hello，spring Boot 2！天源迪科";
+    }
+
+}
