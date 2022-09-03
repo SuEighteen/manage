efea0e411ab7b604d400c8062ef2ecc8e2551160
ohshit <897927040@qq.com>
2022-09-04 00:05:50 +0800

    idea提交

--- src/main/java/com/tydic/boot/controller/HelloController.java
+++ src/main/java/com/tydic/boot/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @RequestMapping("/hello11")
     public String handle01(){
-        return "Hello，spring Boot 2！天源迪科";
+        return "Hello，spring Boot 2！ 1";
     }
 
 }
